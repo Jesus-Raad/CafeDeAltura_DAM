2d9b922bd58cbe6b65ea4a79231f3bad0d5b794d
Nataliia <sokhatska.n@gmail.com>
2026-03-21 19:58:53 +0100

    commin acto Nataliia

--- app/src/main/java/com/example/cafedealtura_dam/data/remote/ProductResponse.kt
+++ app/src/main/java/com/example/cafedealtura_dam/data/remote/ProductResponse.kt
@@ -7,10 +7,10 @@ data class ProductResponse(
     val price: Double,
     val img_url: String,
     val available: Int,
-    val category: String, // Привязка к таблице categories
+    val category: String,
     val origin: String,
     val weight: Int,
-    // Дополнительные поля для макета Detail
+
     val altitude: String = "1,400 - 1,800 msnm",
     val process: String = "Lavado",
     val notes: String = "Chocolate, Caramelo, Nuez"
